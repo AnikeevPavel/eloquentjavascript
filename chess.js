9999095dfd4f5fbf2731624c5d798a5acd4fb588
Pavel Anikeev <delemy@yandex.ru>
2022-07-08 23:58:51 +0300

    Create chess.js

---
+++ chess.js
@@ -0,0 +1,18 @@
+
+function chess (n){
+let res = '';
+for (let i = 0; i<n;i++){
+  if (i % 2 === 0 && n%2===0){
+    res+='# '.repeat(n/2) + '\n';
+  }else if (i % 2 !== 0 && n%2==0) {
+    res+=' #'.repeat(n/2)+ '\n';
+  } else if(i % 2 === 0 && n%2!==0){
+    res+='# '.repeat(n/2)+'#'+ '\n';
+  } else if(i % 2 !== 0 && n%2!==0){
+    res+=' #'.repeat(n/2)+" "+ '\n';
+  }
+}
+return res;
+}
+
+console.log(chess(100))
